244433f166e56d8eed947ea08ac87a8cb2813e4d
Aadit <aadits2004@gmail.com>
2026-05-23 14:44:02 +0530

    rmsnorm

--- pytorch_impl/smol_lm.py
+++ pytorch_impl/smol_lm.py
@@ -47,18 +47,27 @@ def get_batch(split: str, batch_size: int, context_length: int):
 # LayerNorm (with Y(gamma) and B(beta) params to train)
 
 
-class LayerNorm(nn.Module):
-    def __init__(self, dims, eps=1e-5) -> None:
-        super().__init__()
-        self.eps = eps
-        self.weight = nn.Parameter(torch.ones(dims))
-        self.bias = nn.Parameter(torch.zeros(dims))
-
-    def forward(self, x):
-        mean = x.mean(dim=-1, keepdim=True)
-        var = x.var(dim=-1, keepdim=True, unbiased=False)
-        x_norm = (x - mean) / torch.sqrt(var + self.eps)
-        return x_norm * self.weight + self.bias
+# class LayerNorm(nn.Module):
+#     def __init__(self, dims, eps=1e-5) -> None:
+#         super().__init__()
+#         self.eps = eps
+#         self.weight = nn.Parameter(torch.ones(dims))
+#         self.bias = nn.Parameter(torch.zeros(dims))
+#
+#     def forward(self, x):
+#         mean = x.mean(dim=-1, keepdim=True)
+#         var = x.var(dim=-1, keepdim=True, unbiased=False)
+#         x_norm = (x - mean) / torch.sqrt(var + self.eps)
+#         return x_norm * self.weight + self.bias
 
 
 # RMSNorm (Root Mean Square Norm)
+class RMSNorm(nn.Module):
+    def __init__(self, dim, eps=1e-6):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(dim))
+
+    def forward(self, x):
+        rms = torch.sqrt(x.pow(2).mean(dim=-1, keepdim=True) + self.eps)
+        return (x / rms) * self.weight
